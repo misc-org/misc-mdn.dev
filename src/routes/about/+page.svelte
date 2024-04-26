9a861369d790cc5bd50f10295f08d82a77c2d743
shiki-01 <0110shiki@gmail.com>
2024-04-26 20:49:09 +0900

    📁 save

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import Marker from "$lib/components/Marker.svelte";
   import About from "$lib/components/About.svelte";
+  import Marker from "$lib/components/Marker.svelte";
 </script>
 
 <h2>
@@ -15,23 +15,5 @@
   div {
     display: grid;
     place-items: center;
-
-    article {
-      padding-block: $spacing-10;
-      text-align: center;
-      display: flex;
-      flex-direction: column;
-      gap: $spacing-10;
-
-      > section {
-        display: flex;
-        flex-direction: column;
-        gap: $spacing-5;
-
-        > h3 {
-          font-weight: $font-extrabold;
-        }
-      }
-    }
   }
 </style>
