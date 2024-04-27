e1053204f8f7689bf4d7f23cdb55efbb1f450755
shiki-01 <0110shiki@gmail.com>
2024-04-27 21:02:38 +0900

    linkとかうごかした

--- src/lib/utils/types/blogs.ts
+++ src/lib/utils/types/blogs.ts
@@ -1,23 +1,23 @@
 import type { Blogs } from "./microcms";
 
 export type Tag = {
-    title: "All" | Blogs["tags"][number];
-    icon: string;
+  title: "All" | Blogs["tags"][number];
+  icon: string;
 };
 
 export type TagMode = "All" | Blogs["tags"][number];
 
 export type Tags = {
-    All: Tag;
-    [key: string]: Tag;
+  All: Tag;
+  [key: string]: Tag;
 };
 
 export const tags: Tags = {
-    "info": { title: "お知らせ", icon: "mdi:bell" },
-    "blogs": { title: "ブログ", icon: "mdi:book" },
-    "works": { title: "作品紹介", icon: "mdi:palette" },
-    "report": { title: "活動報告", icon: "mdi:account" },
-    "numeric-1": { title: "1 年生", icon: "mdi:numeric-1" },
-    "numeric-2": { title: "2 年生", icon: "mdi:numeric-2" },
-    "numeric-3": { title: "3 年生", icon: "mdi:numeric-3" },
+  info: { title: "お知らせ", icon: "mdi:bell" },
+  blogs: { title: "ブログ", icon: "mdi:book" },
+  works: { title: "作品紹介", icon: "mdi:palette" },
+  report: { title: "活動報告", icon: "mdi:account" },
+  "numeric-1": { title: "1 年生", icon: "mdi:numeric-1" },
+  "numeric-2": { title: "2 年生", icon: "mdi:numeric-2" },
+  "numeric-3": { title: "3 年生", icon: "mdi:numeric-3" },
 };
--- src/lib/utils/types/microcms.ts
+++ src/lib/utils/types/microcms.ts
@@ -13,8 +13,8 @@ type DateType = {
 type Structure<T, P> = T extends "get"
   ? { id: string } & DateType & Required<P>
   : T extends "gets"
-  ? GetsType<{ id: string } & DateType & Required<P>>
-  : Partial<DateType> & (T extends "patch" ? Partial<P> : P);
+    ? GetsType<{ id: string } & DateType & Required<P>>
+    : Partial<DateType> & (T extends "patch" ? Partial<P> : P);
 
 export type Blogs<T = "get"> = Structure<
   T,
