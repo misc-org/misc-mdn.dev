beec1cb3cb3d8cca965bc270fdc80e89681e2480
shiki-01 <0110shiki@gmail.com>
2024-05-26 12:59:41 +0900

    Update +page.server.ts

--- src/routes/blogs/[slug]/+page.server.ts
+++ src/routes/blogs/[slug]/+page.server.ts
@@ -8,22 +8,13 @@ export const load: PageServerLoad = async ({ request, params }) => {
   const { slug } = params;
   const url = new URL(request.url);
   const draftKey = url.searchParams.get("draftKey");
-  try {
-    let res;
-    if (draftKey) {
-      res = await getDraftContentDetail("blogs", slug, draftKey);
-    } else {
-      res = await getContentDetail("blogs", slug);
-    }
-    return {
-      detail: res,
-    };
-  } catch (error: any) {
-    if (error.response?.status === 404) {
-      return {
-        status: 404,
-        error: new Error("Not Found"),
-      };
-    }
+  let res;
+  if (draftKey) {
+    res = await getDraftContentDetail("blogs", slug, draftKey);
+  } else {
+    res = await getContentDetail("blogs", slug);
   }
+  return {
+    detail: res,
+  };
 };
